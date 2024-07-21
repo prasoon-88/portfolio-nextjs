919b9a4517f8cbc8139b4874e799c7c7a1bc7e87
Prasoon Asati <prasouna@gmail.com>
2024-07-21 12:48:22 +0530

    add:lineheight and fontsize scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <main className={sora.className}>
         <div className="container">
           <Button text="Add Me" size="small" />
+          <div className="fs-400">Hello thi</div>
         </div>
       </main>
     </>
