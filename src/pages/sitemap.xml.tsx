95ab6abacf902d4dfd88670399a0aa227664bcf2
Prasoon Asati <prasouna@gmail.com>
2024-07-24 11:22:17 +0530

    update: sitemap.xml

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,39 +1,13 @@
-const baseURL = "https://prasoonasati.me";
+import { MetadataRoute } from "next";
 
-const allPages = [
-  {
-    url: `${baseURL}/`,
-    lastMod: new Date().toISOString(),
-  },
-];
-
-export async function getServerSideProps({ res }: any) {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${allPages
-        .map((ele: any) => {
-          return `
-        <url>
-            <loc>${ele.url}</loc>
-            <lastmod>${ele.lastMod}</lastmod>
-            <changefreq>daily</changefreq>
-            <priority>1.0</priority>
-        </url>
-      `;
-        })
-        .join("")}
-    </urlset>
-  `;
-
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
-
-  return {
-    props: {},
-  };
+export default function sitemap(): MetadataRoute.Sitemap {
+  const BASE = "https://prasoonasati.me/";
+  return [
+    {
+      url: BASE,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
 }
-
-const SiteMap = () => {};
-
-export default SiteMap;
